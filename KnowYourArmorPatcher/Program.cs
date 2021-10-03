e14b22f4ec4cde4edf02ef678b537cc50ce4886f
Sekrit <reaperanon415@gmail.com>
2021-10-03 14:41:54 +0300

    Better mod support
    Added mod support in the form of reading json files with armor rules that share the names of plugins and respect their load order priority.
    Also split IA support into its own file and added support for the guards armor replacer mod as an example.

--- KnowYourArmorPatcher/Program.cs
+++ KnowYourArmorPatcher/Program.cs
@@ -61,6 +61,7 @@ namespace KnowYourArmorPatcher
             // The en-US is to make the whole numbers and decimals split with a . instead of a ,
             if (num != 1) description.Append(" " + name + " x" + Math.Round(num, 2).ToString(new CultureInfo("en-US")) + ",");
         }
+
         /*private static string GenerateDescription(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, string recordEDID, JObject armorRulesJson, float effectIntensity)
         {
             StringBuilder description = new StringBuilder();
@@ -184,13 +185,48 @@ namespace KnowYourArmorPatcher
             if (!state.LoadOrder.ContainsKey(shadowSpellPackage) && state.LoadOrder.ContainsKey(LightAndShadow.ModKey))
                 Console.WriteLine("WARNING: Know Your Enemy Light and Shadow detected, but Shadow Spells Package was not found!");
 
-            string[] requiredFiles = { state.ExtraSettingsDataPath + @"\armor_rules.json", state.ExtraSettingsDataPath + @"\misc.json", state.ExtraSettingsDataPath + @"\settings.json" };
+            /*
+            string[] requiredFiles = { , , state.ExtraSettingsDataPath + @"\settings.json" };
             string[] foundFiles = Directory.GetFiles(state.ExtraSettingsDataPath);
             if (!requiredFiles.SequenceEqual(foundFiles))
                 throw new Exception("Missing required files! Make sure to copy all files over when installing the patcher, and don't run it from within an archive.");
+            */
 
-            var armorRulesJson = JObject.Parse(File.ReadAllText(requiredFiles[0]));
-            var miscJson = JObject.Parse(File.ReadAllText(requiredFiles[1]));
+            // Get all the files in the KYA settings folder and trim the paths.
+            List<string> allFiles = Directory.EnumerateFiles(state.ExtraSettingsDataPath).ToList();
+            if (!allFiles.Any()) throw new Exception("Settings folder for Know Your Armor is empty. Run the patcher again so Synthesis can populate it.");
+            for (int i = 0; i < allFiles.Count; ++i) allFiles[i] = allFiles[i].Substring(allFiles[i].LastIndexOf(@"\") + 1);
+            
+            // Check if the required settings are present or not.
+            if (!allFiles.Contains("armor_rules.json") || !allFiles.Contains("misc.json") || !allFiles.Contains("settings.json"))
+                throw new Exception("Core settings files are missing. Delete the settings folder for Know Your Armor and then run the patcher again so Synthesis can populate it.");
+            allFiles.Remove("armor_rules.json"); allFiles.Remove("misc.json"); allFiles.Remove("settings.json");
+
+            // If there are any additional files, read the armor rules into the rules list while respecting the load order.
+            List<JObject?> armorRules = new();
+            if(allFiles.Any())
+            {
+                Console.WriteLine("\nAdditional Settings Files: ");
+                foreach (var modListing in state.LoadOrder.PriorityOrder)
+                {
+                    string modName = modListing.ModKey.Name + ".json";
+                    if (allFiles.Contains(modName))
+                    {
+                        armorRules.Add(JObject.Parse(File.ReadAllText(state.ExtraSettingsDataPath + @"\" + modName)));
+                        Console.WriteLine(" - " + modName + " - PLUGIN PRESENT");
+                        allFiles.Remove(modName);
+                    }
+                }
+
+                foreach (var missingMod in allFiles)
+                    Console.WriteLine(" - " + missingMod + " - PLUGIN MISSING");
+                Console.WriteLine();
+            }
+
+            // Add the base armor rules file to the end of the rules list.
+            armorRules.Add(JObject.Parse(File.ReadAllText(state.ExtraSettingsDataPath + @"\armor_rules.json")));
+            var miscJson = JObject.Parse(File.ReadAllText(state.ExtraSettingsDataPath + @"\misc.json"));
+            //var armorRulesJson = JObject.Parse(File.ReadAllText(requiredFiles[0]));
             //var settingsJson = JObject.Parse(File.ReadAllText(requiredFiles[2]));
 
             // Converting to list because .Contains in Newtonsoft.JSON has weird behavior
@@ -200,16 +236,15 @@ namespace KnowYourArmorPatcher
             float effectIntensity = _settings.Value.EffectIntensity;
             //bool patchArmorDescriptions = _settings.Value.PatchArmorDescriptions;
 
-            Console.WriteLine("*** DETECTED SETTINGS ***");
+            Console.WriteLine("Settings Used: ");
             //Console.WriteLine("patch_armor_descriptions: " + patchArmorDescriptions);
-            Console.WriteLine("effect_intensity: " + effectIntensity);
-            Console.WriteLine("*************************");
+            Console.WriteLine("effect_intensity: " + effectIntensity + "\n");
 
             if (!KnowYourEnemy.Perk.kye_perk_armors2.TryResolve(state.LinkCache, out var perkLink))
                 throw new Exception($"Unable to find required perk: {KnowYourEnemy.Perk.kye_perk_armors2}");
 
             // Returns all keywords from an armor that are found in armor rules json 
-            List<string> GetRecognizedKeywords(IArmorGetter armor)
+            List<string> GetRecognizedKeywords(IArmorGetter armor, JObject armorRuleSet)
             {
                 List<string> foundEDIDs = new List<string>();
                 if (armor.Keywords == null) return foundEDIDs;
@@ -217,7 +252,7 @@ namespace KnowYourArmorPatcher
                 {
                     if (keyword.TryResolve(state.LinkCache, out var kw))
                     {
-                        if (kw.EditorID != null && armorRulesJson![kw.EditorID] != null)
+                        if (kw.EditorID != null && armorRuleSet![kw.EditorID] != null)
                         {
                             // Make sure ArmorMaterialIron comes first - fixes weird edge case generating descriptions when ArmorMaterialIronBanded is also in there
                             if (kw.Equals(Skyrim.Keyword.ArmorMaterialIronBanded))
@@ -277,84 +312,89 @@ namespace KnowYourArmorPatcher
                 if (armor.Keywords == null || !armor.Keywords.Contains(Skyrim.Keyword.ArmorCuirass)) continue;
                 if (!armor.TemplateArmor.IsNull) continue;
 
-                List<string> foundEDIDs = GetRecognizedKeywords(armor);
-                if (armorRulesJson[armor.EditorID] == null && !foundEDIDs.Any()) continue;
-
-                List<string> armorKeywordsToAdd = new List<string>();
-
-                var armorCopy = state.PatchMod.Armors.GetOrAddAsOverride(armor);
-                var origDescription = armorCopy.Description;
-                foreach (string foundEDID in foundEDIDs)
+                foreach (var armorRuleSet in armorRules)
                 {
-                    // Get KYE keywords connected to recognized armor keyword
-                    foreach (string keywordToAdd in ((JArray)armorRulesJson[foundEDID]!).ToObject<string[]>()!)
+                    if (armorRuleSet is null) continue;
+                    List<string> foundEDIDs = GetRecognizedKeywords(armor, armorRuleSet);
+                    if (armorRuleSet[armor.EditorID] == null && !foundEDIDs.Any()) continue;
+
+                    List<string> armorKeywordsToAdd = new List<string>();
+
+                    var armorCopy = state.PatchMod.Armors.GetOrAddAsOverride(armor);
+                    //var origDescription = armorCopy.Description;
+                    foreach (string foundEDID in foundEDIDs)
                     {
-                        if (!armorKeywordsToAdd.Contains(keywordToAdd))
-                            armorKeywordsToAdd.Add(keywordToAdd);
-                    }
-                    /*if (patchArmorDescriptions)
-                    {
-                        string desc = GenerateDescription(state, foundEDID, armorRulesJson, effectIntensity);
-                        if (!String.IsNullOrEmpty(desc))
+                        // Get KYE keywords connected to recognized armor keyword
+                        foreach (string keywordToAdd in ((JArray)armorRuleSet[foundEDID]!).ToObject<string[]>()!)
                         {
-                            if (armorCopy.Description?.String.IsNullOrEmpty() ?? true)
+                            if (!armorKeywordsToAdd.Contains(keywordToAdd))
+                                armorKeywordsToAdd.Add(keywordToAdd);
+                        }
+                        /*if (patchArmorDescriptions)
+                        {
+                            string desc = GenerateDescription(state, foundEDID, armorRulesJson, effectIntensity);
+                            if (!String.IsNullOrEmpty(desc))
                             {
-                                armorCopy.Description = desc;
-                            }
-                            else
-                            {
-                                if (armorCopy.Description?.String?.EndsWith(".") ?? false)
+                                if (armorCopy.Description?.String.IsNullOrEmpty() ?? true)
                                 {
-                                    armorCopy.Description = origDescription?.String + " " + desc;
+                                    armorCopy.Description = desc;
                                 }
                                 else
                                 {
-                                    armorCopy.Description = origDescription?.String + ". " + desc;
+                                    if (armorCopy.Description?.String?.EndsWith(".") ?? false)
+                                    {
+                                        armorCopy.Description = origDescription?.String + " " + desc;
+                                    }
+                                    else
+                                    {
+                                        armorCopy.Description = origDescription?.String + ". " + desc;
+                                    }
                                 }
                             }
-                        }
-                    }*/
-                }
-
-                if (armorRulesJson[armor.EditorID] != null)
-                {
-                    foreach (string? keywordToAdd in ((JArray)armorRulesJson[armor.EditorID]!).ToObject<string[]>()!)
-                    {
-                        if (keywordToAdd != null && !armorKeywordsToAdd.Contains(keywordToAdd))
-                        {
-                            armorKeywordsToAdd.Add(keywordToAdd);
-                        }
-
+                        }*/
                     }
 
-                    /*if (patchArmorDescriptions)
+                    if (armorRuleSet[armor.EditorID] != null)
                     {
-                        var desc = GenerateDescription(state, armor.EditorID, armorRulesJson, effectIntensity);
-                        if (!String.IsNullOrEmpty(desc))
+                        foreach (string? keywordToAdd in ((JArray)armorRuleSet[armor.EditorID]!).ToObject<string[]>()!)
                         {
-                            if (armorCopy.Description?.String.IsNullOrEmpty() ?? true)
+                            if (keywordToAdd != null && !armorKeywordsToAdd.Contains(keywordToAdd))
                             {
-                                armorCopy.Description = desc;
+                                armorKeywordsToAdd.Add(keywordToAdd);
                             }
-                            else
+
+                        }
+
+                        /*if (patchArmorDescriptions)
+                        {
+                            var desc = GenerateDescription(state, armor.EditorID, armorRulesJson, effectIntensity);
+                            if (!String.IsNullOrEmpty(desc))
                             {
-                                if (armorCopy.Description?.String?.EndsWith(".") ?? false)
+                                if (armorCopy.Description?.String.IsNullOrEmpty() ?? true)
                                 {
-                                    armorCopy.Description = origDescription?.String + " " + desc;
+                                    armorCopy.Description = desc;
                                 }
                                 else
                                 {
-                                    armorCopy.Description = origDescription?.String + ". " + desc;
+                                    if (armorCopy.Description?.String?.EndsWith(".") ?? false)
+                                    {
+                                        armorCopy.Description = origDescription?.String + " " + desc;
+                                    }
+                                    else
+                                    {
+                                        armorCopy.Description = origDescription?.String + ". " + desc;
+                                    }
                                 }
                             }
-                        }
-                    }*/
-                }
+                        }*/
+                    }
 
-                // Add keywords that are to be added to armor
-                foreach (var keyword in armorKeywordsToAdd)
-                {
-                    armorCopy.Keywords!.Add(armorKeywords[keyword]);
+                    // Add keywords that are to be added to armor
+                    foreach (var keyword in armorKeywordsToAdd)
+                    {
+                        armorCopy.Keywords!.Add(armorKeywords[keyword]);
+                    }
+                    break;
                 }
             }
         }
